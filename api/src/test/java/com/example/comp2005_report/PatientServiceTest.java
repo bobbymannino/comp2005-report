9f6e9fc3b178fc30817140d39369683634978e73
Bobby Mannino <manninobobby@icloud.com>
2025-03-25 20:48:23 +0000

    api: add patient endpoint test

---
+++ api/src/test/java/com/example/comp2005_report/PatientServiceTest.java
@@ -0,0 +1,39 @@
+package com.example.comp2005_report;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(PatientService.class)
+class PatientServiceTest {
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testGetMultiStaffPatients() throws Exception {
+        // arrange
+        MockHttpServletRequestBuilder req = MockMvcRequestBuilders.get("/patients/multi-staff").accept(MediaType.APPLICATION_JSON);
+        String reg = "^\\{\"patients\":\\[\\d?,?(\\d,)*]}$";
+
+        // act
+        MvcResult res = mockMvc.perform(req).andReturn();
+        String resContent = res.getResponse().getContentAsString();
+
+        System.out.println(resContent);
+
+        // assert
+        assertEquals(200, res.getResponse().getStatus());
+        assertNotNull(resContent);
+        assertTrue(resContent.matches(reg));
+    }
+}
