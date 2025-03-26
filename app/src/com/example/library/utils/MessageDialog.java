efa6606acffc4dad9c3228e3d8b2efedc634874d
Bobby Mannino <manninobobby@icloud.com>
2025-03-26 13:55:50 +0000

    app: add info message dialog

--- app/src/com/example/library/utils/MessageDialog.java
+++ app/src/com/example/library/utils/MessageDialog.java
@@ -5,13 +5,19 @@ import javax.swing.*;
 public class MessageDialog {
     public static void showError(String message, JPanel contentPane) {
         SwingUtilities.invokeLater(() -> {
-            JOptionPane.showMessageDialog(contentPane, message, "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(contentPane, message, "Error!", JOptionPane.ERROR_MESSAGE);
         });
     }
 
     public static void showWarning(String message, JPanel contentPane) {
         SwingUtilities.invokeLater(() -> {
-            JOptionPane.showMessageDialog(contentPane, message, "Warning", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(contentPane, message, "Warning!", JOptionPane.WARNING_MESSAGE);
+        });
+    }
+
+    public static void showInfo(String message, JPanel contentPane) {
+        SwingUtilities.invokeLater(() -> {
+            JOptionPane.showMessageDialog(contentPane, message, "Info", JOptionPane.INFORMATION_MESSAGE);
         });
     }
 }
