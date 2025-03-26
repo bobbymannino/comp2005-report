c2b53d62376b11edccb0c3a57b74fdc9b116aeea
Bobby Mannino <manninobobby@icloud.com>
2025-03-26 14:58:41 +0000

    app: make `Patient` more secure

--- app/src/com/example/library/records/Patient.java
+++ app/src/com/example/library/records/Patient.java
@@ -3,22 +3,18 @@ package com.example.library.records;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Patient {
-    public Integer id;
-    public String lastName;
-    public String firstName;
-    public String nhsNumber;
+    public final Integer id;
+    public final String lastName;
+    public final String firstName;
+    public final String nhsNumber;
 
     private final static String PATIENT_NAME_NO_NAME = "UNKNOWN";
 
     public Patient(
-            @JsonProperty("id")
-        Integer id,
-        @JsonProperty("surname")
-        String lastName,
-            @JsonProperty("forename")
-        String firstName,
-            @JsonProperty("nhsNumber")
-        String nhsNumber
+            @JsonProperty("id") Integer id,
+            @JsonProperty("surname") String lastName,
+            @JsonProperty("forename") String firstName,
+            @JsonProperty("nhsNumber") String nhsNumber
     ) {
         this.id = id;
         this.nhsNumber = nhsNumber;
